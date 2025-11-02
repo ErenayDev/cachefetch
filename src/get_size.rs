a6fb738c8cc55c6c717962d0713e2301856d9360
ErenayDev <erenaydev@proton.me>
2025-11-02 12:05:10 +0300

    feat: add support for different OS

--- src/get_size.rs
+++ src/get_size.rs
@@ -2,13 +2,25 @@ use std::{env, fs, path::Path};
 
 pub fn get_size(path: &Path) -> std::io::Result<u64> {
     let expanded_path = if path.starts_with("~") {
-        let home = env::var("HOME").map_err(|_| {
+        if let Ok(home) = env::var("HOME") {
+            Path::new(&home).join(path.strip_prefix("~").unwrap())
+        } else if let Ok(home) = env::var("USERPROFILE") {
+            Path::new(&home).join(path.strip_prefix("~").unwrap())
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "HOME/USERPROFILE environment variable not found",
+            ));
+        }
+    } else if path.starts_with("%") && path.ends_with("%") {
+        let var_name = path.to_str().unwrap().trim_matches('%');
+        let var_value = env::var(var_name).map_err(|_| {
             std::io::Error::new(
                 std::io::ErrorKind::NotFound,
-                "HOME environment variable not found",
+                format!("{} environment variable not found", var_name),
             )
         })?;
-        Path::new(&home).join(path.strip_prefix("~").unwrap())
+        Path::new(&var_value).to_path_buf()
     } else {
         path.to_path_buf()
     };
@@ -17,18 +29,22 @@ pub fn get_size(path: &Path) -> std::io::Result<u64> {
     if metadata.is_file() {
         Ok(metadata.len())
     } else if metadata.is_dir() {
-        let mut total = 0;
-        if let Ok(entries) = fs::read_dir(&expanded_path) {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    if let Ok(size) = get_size(&entry.path()) {
-                        total += size;
-                    }
-                }
-            }
-        }
-        Ok(total)
+        calculate_dir_size(&expanded_path)
     } else {
         Ok(0)
     }
+}
+
+fn calculate_dir_size(path: &Path) -> std::io::Result<u64> {
+    let mut total = 0;
+    if let Ok(entries) = fs::read_dir(path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(size) = get_size(&entry.path()) {
+                    total += size;
+                }
+            }
+        }
+    }
+    Ok(total)
 }
